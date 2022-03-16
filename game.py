f753ae7c3165bff9f33f31dba443d7088029b26d
NOZARA Marotia Dodson <45108283+marotia29@users.noreply.github.com>
2022-03-16 15:43:55 +0300

    Update game.py

--- game.py
+++ game.py
@@ -9,15 +9,14 @@ class Game:
             self.grid.append(random.choice(string.ascii_uppercase))
             
     def is_valid(self, word):
-        # if not word:
-        #     return False
-        # letters = self.grid.copy()
-        # for letter in word:
-        #     if letter in letters:
-        #         letters.remove(letter)
-        #     else:
-        #         return False
-        # return True
+        if not word:
+            return False
+        letters = self.grid.copy()
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
         return self.__check_dictionary(word)
     
     @staticmethod
